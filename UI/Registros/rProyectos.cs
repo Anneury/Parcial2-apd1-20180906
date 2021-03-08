14f08bb71dff5c02a550c81239bf32108cc2baee
Anneury <Anneury@DESKTOP-AAVNCBB>
2021-03-08 17:09:25 -0400

    Agregando todas las ventanas necesarias. Closes #6.

---
+++ UI/Registros/rProyectos.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Parcial2_apd1_20180906.UI.Registros
+{
+    public partial class rProyectos : Form
+    {
+        public rProyectos()
+        {
+            InitializeComponent();
+        }
+    }
+}
