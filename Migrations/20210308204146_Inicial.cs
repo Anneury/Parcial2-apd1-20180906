14f9e20eb9d8320388355f4d287b848f1ef6b58a
Anneury <Anneury@DESKTOP-AAVNCBB>
2021-03-08 16:43:09 -0400

    Creando las migraciones y la base de datos. Closes #4.

---
+++ Migrations/20210308204146_Inicial.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Parcial2_apd1_20180906.Migrations
+{
+    public partial class Inicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Proyectos",
+                columns: table => new
+                {
+                    TipoId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Fecha = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    DescripcionProyecto = table.Column<string>(type: "TEXT", nullable: true),
+                    TiempoTotal = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Proyectos", x => x.TipoId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tareas",
+                columns: table => new
+                {
+                    TareaId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    TipoTarea = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tareas", x => x.TareaId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ProyectosDetalle",
+                columns: table => new
+                {
+                    TipoId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    TipoTarea = table.Column<string>(type: "TEXT", nullable: true),
+                    Requerimiento = table.Column<string>(type: "TEXT", nullable: true),
+                    Tiempo = table.Column<int>(type: "INTEGER", nullable: false),
+                    ProyectoId = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProyectosDetalle", x => x.TipoId);
+                    table.ForeignKey(
+                        name: "FK_ProyectosDetalle_Proyectos_ProyectoId",
+                        column: x => x.ProyectoId,
+                        principalTable: "Proyectos",
+                        principalColumn: "TipoId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Tareas",
+                columns: new[] { "TareaId", "TipoTarea" },
+                values: new object[] { 1, "Analisis" });
+
+            migrationBuilder.InsertData(
+                table: "Tareas",
+                columns: new[] { "TareaId", "TipoTarea" },
+                values: new object[] { 2, "Diseño" });
+
+            migrationBuilder.InsertData(
+                table: "Tareas",
+                columns: new[] { "TareaId", "TipoTarea" },
+                values: new object[] { 3, "Programcion" });
+
+            migrationBuilder.InsertData(
+                table: "Tareas",
+                columns: new[] { "TareaId", "TipoTarea" },
+                values: new object[] { 4, "Prueba" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProyectosDetalle_ProyectoId",
+                table: "ProyectosDetalle",
+                column: "ProyectoId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ProyectosDetalle");
+
+            migrationBuilder.DropTable(
+                name: "Tareas");
+
+            migrationBuilder.DropTable(
+                name: "Proyectos");
+        }
+    }
+}
