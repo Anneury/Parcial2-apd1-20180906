5a743e4109541c08a23aeed856b87f767e922911
Anneury <Anneury@DESKTOP-AAVNCBB>
2021-03-08 16:46:33 -0400

    Agregando las clases a la carpeta BLL sin programar, y ademas agregando los using

---
+++ BLL/TareasBLL.cs
@@ -0,0 +1,12 @@
+﻿using Parcial2_apd1_20180906.Entidades;
+using Parcial2_apd1_20180906.DAL;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Parcial2_apd1_20180906.BLL
+{
+    public class TareasBLL
+    {
+    }
+}
