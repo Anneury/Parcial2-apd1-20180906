3cf5a19bf5d2ec3760c1dc878563e5094860d742
Anneury <Anneury@DESKTOP-AAVNCBB>
2021-03-08 16:52:33 -0400

    Agregando las clases a la BLL sin programar. Closes #5.

--- BLL/TareasBLL.cs
+++ BLL/TareasBLL.cs
@@ -1,8 +1,8 @@
-﻿using Parcial2_apd1_20180906.Entidades;
-using Parcial2_apd1_20180906.DAL;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Parcial2_apd1_20180906.Entidades;
+using Parcial2_apd1_20180906.DAL;
 
 namespace Parcial2_apd1_20180906.BLL
 {
